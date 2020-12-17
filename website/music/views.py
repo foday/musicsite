6f903056c73d538f9e8f569e7446eeeec8ef9da1
foday <foday_s@yahoo.com>
2020-12-17 15:42:04 -0600

    Continue....Add url path, add function to music/views file, create music/urls.py file
    Add the music app file so when someone goes to my website it will go to the main urls then go to the music url if music is typed at the end of the url address. The function index(request) with a request parameter is making a http response which is an html content..'hello world' also 'from django.http import HttpResponse' was added, but before that I made a music\urls.py file or a urls.py file for the music app. Then I add the imports and then the urlpatterns.
    So when someone comes to my site and types www.music.com they will first go to the 'website' urls to look for the urlpatterns then next it will see the music path and go to the music urls look at the pattern then go to views file for the music app look at the index function and execute that function when is send an http response

--- website/music/views.py
+++ website/music/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here & your functions
 def index(request):
     HttpResponse("<h1>hello world</h1>")
